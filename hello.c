d6e35d867b18b01e0e4188446c2f25773d9fe865
nothub <48992448+nothub@users.noreply.github.com>
2021-12-30 18:54:13 +0100

    write types

--- hello.c
+++ hello.c
@@ -1,4 +1,4 @@
-int write(int fd, const void *buf, int count);
+long int write(int fd, const void *buf, long unsigned int count);
 
 int main(int argc, char *argv[], char* envp[]) {
     write(1, "Henlo World! :3\n", 16);
