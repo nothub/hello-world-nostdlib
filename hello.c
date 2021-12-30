bd7bc64b0e42b18d5d776b5098468222e1cddef9
nothub <48992448+nothub@users.noreply.github.com>
2021-12-30 03:04:26 +0100

    write string length

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
-int write(int fd, void *bug, int count);
+int write(int fd, const void *buf, int count);  /* declaring int count instead of size_t count is a lazy hack to skip some compiler warnings */
 
 int main(int argc, char *argv[], char* envp[]) {
-    write(1, "hi :3\n", 6);
+    write(1, "Henlo World! :3\n", 16);
     return 0;
 }
