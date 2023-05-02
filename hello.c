c6db24f4d632d338e965f5aa513593439aeda9a3
Florian Hübner <code@hub.lol>
2023-05-02 22:34:13 +0200

    unused args

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 long int write(int fd, const void *buf, long unsigned int count);
 
-int main(int argc, char *argv[], char* envp[]) {
+int main() {
     write(1, "Hello, World!\n", 14);
     return 0;
 }
